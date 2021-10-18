53ede9405d0a040efd96a61543bc130888e60c73
University <f.lewis20.class18@outlook.com>
2021-10-18 01:49:38 +0000

    backend/src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
+import ejs from 'ejs'
 const app = express()
 const port = process.env.PORT || 4000
 const baseUrl = `http://localhost:${port}`
 
+app.set('views', path.join(__dirname, '../views '))
+app.set('view engine', 'ejs')
 app.get('/1972/09/11', (req, res) => {
-  res.status(200).send('Hello world!')
+  res.status(200).render('pages/index')
 })
 
 // Server
