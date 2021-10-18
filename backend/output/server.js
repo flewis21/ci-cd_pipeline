b13b1b9224203bc53ff4b6fdbc1cc37caabc3cd5
flewis21 <fabian.lewis@outlook.com>
2021-10-18 00:24:04 -0400

    backend/output/server.js

--- backend/output/server.js
+++ backend/output/server.js
@@ -8,10 +8,10 @@ const path1 = __importDefault(require('path'))
 const app = (0, express1.default)()
 const port = process.env.PORT || 4000
 const baseUrl = `http://localhost:${port}`
-app.set('views', path1.default.join(__dirname, '../views/ '))
+app.set('views', path1.default.resolve(__dirname, '../views/pages '))
 app.set('view engine', 'ejs')
 app.get('/1972/09/11', (req, res) => {
-  res.status(200).render('pages/index')
+  res.status(200).render('index')
 })
 // Server
 app.listen(port, () => {
