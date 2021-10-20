58e4b0ac42a166507999429a22dee6bfe7daf02b
flewis21 <fabian.lewis@outlook.com>
2021-10-20 13:26:45 -0400

    frontend/src/macros.ts

--- frontend/src/macros.ts
+++ frontend/src/macros.ts
@@ -4,7 +4,7 @@ function Header() {
   var spreadsheet = SpreadsheetApp.getActive();
   var sheet = spreadsheet.getActiveSheet();
   sheet.getRange(spreadsheet.getCurrentCell().getRow(), 1, 1, 10).activate();
-  spreadsheet.getActiveRangeList().setBackground('#4c1130')
+  spreadsheet.getActiveRangeList().setBackground('#afeeee')
   .setFontColor('#ffffff')
   .setFontWeight('bold');
   spreadsheet.getActiveSheet().setFrozenRows(1);
