ca55ffef36798b18ca082b7199b678ab2e3164e5
flewis21 <fabian.lewis@outlook.com>
2021-10-20 14:45:02 -0400

    src/customFunctions.ts

--- frontend/src/customFunctions.ts
+++ frontend/src/customFunctions.ts
@@ -1,11 +1,12 @@
+// Compiled using ci-cd_pipeline 1.0.0 (TypeScript 4.4.4)
 /**
  * Converts US dollars to Swiss francs.
  *
  * @param {number} dollars The total number of dollars.
- * @param {number} swissFrancs The converted total of Swiss francs.
+ * @return {number} swissFrancs The converted total of Swiss francs.
  * @customFunction
  */
 function USDTOCHF(dollars) {
-	var swissFrancs = dollars * .99;
-	return swissFrancs;
+    var swissFrancs = dollars * .99;
+    return swissFrancs;
 }
