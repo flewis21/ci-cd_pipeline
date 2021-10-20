74881866c74858ab54bc8477f6da318f2f508553
flewis21 <fabian.lewis@outlook.com>
2021-10-20 14:45:19 -0400

    src/macros.ts

--- frontend/src/macros.ts
+++ frontend/src/macros.ts
@@ -1,11 +1,12 @@
+// Compiled using ci-cd_pipeline 1.0.0 (TypeScript 4.4.4)
 /** @OnlyCurrentDoc */
-
 function Header() {
-  var spreadsheet = SpreadsheetApp.getActive();
-  var sheet = spreadsheet.getActiveSheet();
-  sheet.getRange(spreadsheet.getCurrentCell().getRow(), 1, 1, 10).activate();
-  spreadsheet.getActiveRangeList().setBackground('#afeeee')
-  .setFontColor('#191970')
-  .setFontWeight('bold');
-  spreadsheet.getActiveSheet().setFrozenRows(1);
-};
+    var spreadsheet = SpreadsheetApp.getActive();
+    var sheet = spreadsheet.getActiveSheet();
+    sheet.getRange(spreadsheet.getCurrentCell().getRow(), 1, 1, 10).activate();
+    spreadsheet.getActiveRangeList().setBackground('#afeeee')
+        .setFontColor('#191970')
+        .setFontWeight('bold');
+    spreadsheet.getActiveSheet().setFrozenRows(1);
+}
+;
