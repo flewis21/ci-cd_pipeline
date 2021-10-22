7d712265fe8568b75ad4538e96faf792d7cb8928
flewis21 <fabian.lewis@outlook.com>
2021-10-22 12:14:08 -0400

    frontend/macros_and_custom_functions/src/renameSpreadsheet.ts

--- frontend/macros_and_custom_functions/src/renameSpreadsheet.ts
+++ frontend/macros_and_custom_functions/src/renameSpreadsheet.ts
@@ -2,9 +2,3 @@ function renameSpreadsheet() {
 	var mySS = SpreadsheetApp.getActiveSpreadsheet();
 	mySS.rename("Define Sipoc and create value stream")
 }
-
-function duplicateAndOrganizeActiveSheet() {
-	var mySS = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Start');
-		mySS.activate();
-	var duplicatesheet = mySS.duplicateActiveSheet();
-}
