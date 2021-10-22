bbadbfdff0d6b3d417d9998334c3a798c67a32e3
flewis21 <fabian.lewis@outlook.com>
2021-10-22 12:20:05 -0400

    frontend/macros_and_custom_functions/src/duplicateSpreadsheet.ts

---
+++ frontend/macros_and_custom_functions/src/duplicateSpreadsheet.ts
@@ -0,0 +1,5 @@
+function duplicateAndOrganizeActiveSheet() {
+	var mySS = SpreadsheetApp.getActiveSpreadsheet();
+	var activeSheet = SpreadsheetApp.getActiveSheet();
+	var duplicateSheet = mySS.duplicateActiveSheet();
+}
