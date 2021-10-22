dcc9381b140e3845070850a08dab0cce4898cb02
flewis21 <fabian.lewis@outlook.com>
2021-10-22 12:33:37 -0400

    frontend/macros_and_custom_functions/src/duplicateSpreadsheet.ts

--- frontend/macros_and_custom_functions/src/duplicateSpreadsheet.ts
+++ frontend/macros_and_custom_functions/src/duplicateSpreadsheet.ts
@@ -2,4 +2,7 @@ function duplicateAndOrganizeActiveSheet() {
 	var mySS = SpreadsheetApp.getActiveSpreadsheet();
 	var activeSheet = SpreadsheetApp.getActiveSheet();
 	var duplicateSheet = mySS.duplicateActiveSheet();
+
+	// Change the name of the new sheet.
+	duplicateSheet.setName('Sheet_' + duplicateSheet.getSheetId());
 }
