a091a0c2d1702d8611c680b59aaea882be0a379f
flewis21 <fabian.lewis@outlook.com>
2021-10-25 01:38:48 -0400

    src/index.ts

--- simpleGit/src/index.ts
+++ simpleGit/src/index.ts
@@ -1,7 +1,7 @@
 import * as simpleGit from 'simple-git/promise';
 
 async function build(): Promise<void> {
-  const typeof git: simpleGit = simpleGit();
+  const git: typeof simpleGit = simpleGit();
 
   // list commits
   // git log accepts an options object - from ts definition
