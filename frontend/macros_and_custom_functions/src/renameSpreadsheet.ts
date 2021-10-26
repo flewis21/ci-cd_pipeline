3f1a934d8ed6d2c8d4c647f1467d9aabf3e2e2ce
flewis21 <fabian.lewis@outlook.com>
2021-10-25 21:24:15 -0400

    frontend/macros_and_custom_functions/src/renameSpreadsheet.ts

--- frontend/macros_and_custom_functions/src/renameSpreadsheet.ts
+++ frontend/macros_and_custom_functions/src/renameSpreadsheet.ts
@@ -1,5 +1,5 @@
 /* eslint-env es2017 */
-/* global renameSpreadsheet:writable */
+/* eslint no-unused-vars: ["error", { "caughtErrorsIgnorePattern": "renameSpreadsheet" }] */
 function renameSpreadsheet() {
   var mySS = SpreadsheetApp.getActiveSpreadsheet();
   mySS.rename('Define Sipoc and create value stream');
