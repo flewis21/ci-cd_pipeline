e86d91d309607b495199a39400b1e19acbdff63c
flewis21 <fabian.lewis@outlook.com>
2021-10-25 21:39:20 -0400

    frontend/macros_and_custom_functions/src/renameSpreadsheet.ts

--- frontend/macros_and_custom_functions/src/renameSpreadsheet.ts
+++ frontend/macros_and_custom_functions/src/renameSpreadsheet.ts
@@ -1,10 +1,5 @@
-(function () {
-  /* global renameSpreadsheet */
-  /* eslint-env es2017 */
-  /* eslint no-unused-vars: ["error", { "caughtErrorsIgnorePattern": "renameSpreadsheet" }] */
-  function renameSpreadsheet() {
-    var mySS = SpreadsheetApp.getActiveSpreadsheet();
-    mySS.rename('Define Sipoc and create value stream');
-  }
-  window.renameSpreadsheet = renameSpreadsheet;
-})();
+function renameSpreadsheet() {
+  var mySS = SpreadsheetApp.getActiveSpreadsheet();
+  mySS.rename('Define Sipoc and create value stream');
+}
+window.renameSpreadsheet = renameSpreadsheet;
