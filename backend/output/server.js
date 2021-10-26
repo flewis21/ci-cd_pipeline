1612e451774a4ca7a5c2948332f1abe7599bd9f1
flewis21 <fabian.lewis@outlook.com>
2021-10-26 12:22:34 -0400

    .

--- backend/output/server.js
+++ backend/output/server.js
@@ -1,21 +1,23 @@
-'use strict'
-const __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { default: mod }
-}
-Object.defineProperty(exports, '__esModule', { value: true })
-const express1 = __importDefault(require('express'))
-const serverlesshttp1 = __importDefault(require('serverless-http'))
-const path1 = __importDefault(require('path'))
-const app = (0, express1.default)()
-const port = process.env.PORT || 4000
-const baseUrl = `http://localhost:${port}`
-app.set('views', path1.default.resolve(__dirname, '../views/pages/'))
-app.set('view engine', 'ejs')
+'use strict';
+const __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : {default: mod};
+  };
+Object.defineProperty(exports, '__esModule', {value: true});
+const express1 = __importDefault(require('express'));
+const serverlesshttp1 = __importDefault(require('serverless-http'));
+const path1 = __importDefault(require('path'));
+const app = (0, express1.default)();
+const port = process.env.PORT || 4000;
+const baseUrl = `http://localhost:${port}`;
+app.set('views', path1.default.resolve(__dirname, '../views/pages/'));
+app.set('view engine', 'ejs');
 app.get('/1972/09/11', (req, res, next) => {
-  res.status(200).render('index')
-})
+  res.status(200).render('index');
+});
 // Server
 app.listen(port, () => {
-  console.log('Listening on:', baseUrl)
-})
-module.exports.handler = (0, serverlesshttp1.default)(app)
+  console.log('Listening on:', baseUrl);
+});
+module.exports.handler = (0, serverlesshttp1.default)(app);
