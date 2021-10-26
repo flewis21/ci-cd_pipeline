26545f9cbb8b5537964fc4d3ac1f39d61d07960d
flewis21 <fabian.lewis@outlook.com>
2021-10-25 22:08:30 -0400

    frontend/macros_and_custom_functions/src/macros.ts

--- frontend/macros_and_custom_functions/src/macros.ts
+++ frontend/macros_and_custom_functions/src/macros.ts
@@ -5,4 +5,3 @@ function Header() {
   spreadsheet.getActiveRangeList().setBackground('#afeeee').setFontColor('#191970').setFontWeight('bold');
   spreadsheet.getActiveSheet().setFrozenRows(1);
 }
-window.Header = Header;
