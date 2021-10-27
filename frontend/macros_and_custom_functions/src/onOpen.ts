0763bf4c5728f38120084a4b1b3fd0276a258824
flewis21 <fabian.lewis@outlook.com>
2021-10-27 14:39:06 -0400

    macros_and_custom_functions/src/onOpen.ts

--- frontend/macros_and_custom_functions/src/onOpen.ts
+++ frontend/macros_and_custom_functions/src/onOpen.ts
@@ -83,9 +83,9 @@ function splitAtFirstComma() {
 
 /** 
  * Reformats title and author columns by splitting the title column
- * at the first comma, if present.
+ * at the last instance of the string " by ", if present.
  */
-function splitAtFirstComma() {
+function splitAtLastBy() {
 	// Get the active (currently highlighted) range.
 	var activeRange = SpreadsheetApp.getActiveRange();
 	var titleAuthorRange = activeRange.offset(
@@ -96,24 +96,25 @@ function splitAtFirstComma() {
 	// This is a 2D array.
 	var titleAuthorValues = titleAuthorRange.getValues();
 
-	// Update values where commas are found. Assumes the presence
-	// of a comma indicates an "authors, title" pattern.
+	// Update values where " by " substrings are found. Assumes 
+	// the presence of a " by " indicates a "title by authors" 
+	// pattern.
 	for (var row = 0; row < titleAuthorValues.length; row++) {
-		var indexOfFirstComma =
-			titleAuthorValues[row][0].indexOf(", ");
+		var indexOfLastBy =
+			titleAuthorValues[row][0].lastIndexOf(" by ");
 
-		if(indexOfFirstComma >= 0) {
-			// Found a comma, so split and update the values in
+		if(indexOfLastBy >= 0) {
+			// Found a " by ", so split and update the values in
 			// the values array.
 			var titlesAndAuthors = titleAuthorValues[row][0];
 
 			// Update the title value in the array.
 			titleAuthorValues[row][0] =
-				titlesAndAuthors.slice(indexOfFirstComma + 2);
+				titlesAndAuthors.slice(0, indexOfLastBy);
 
 			// Update the author value in the array.
 			titleAuthorValues[row][1] =
-				titlesAndAuthors.slice(0, indexOfFirstComma);
+				titlesAndAuthors.slice(indexOfLastBy + 4);
 		}
 	}
 
