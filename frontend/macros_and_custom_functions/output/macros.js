4a4f0d9a87e11832b98223a128b2f6ce88db1d6d
flewis21 <80494698+flewis21@users.noreply.github.com>
2021-10-26 21:49:54 -0400

    Delete frontend/macros_and_custom_functions/output directory

--- frontend/macros_and_custom_functions/output/macros.js
+++
@@ -1,9 +0,0 @@
-// Compiled using ci-cd_pipeline 1.0.0 (TypeScript 4.4.4)
-/** @OnlyCurrentDoc */
-function Header() {
-  const spreadsheet = SpreadsheetApp.getActive();
-  const sheet = spreadsheet.getActiveSheet();
-  sheet.getRange(spreadsheet.getCurrentCell().getRow(), 1, 1, 10).activate();
-  spreadsheet.getActiveRangeList().setBackground('#afeeee').setFontColor('#191970').setFontWeight('bold');
-  spreadsheet.getActiveSheet().setFrozenRows(1);
-}
