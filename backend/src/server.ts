b6dcee06b303e42259eb02f70efaa6cfdf425373
flewis21 <fabian.lewis@outlook.com>
2021-10-30 18:36:22 -0400

    backend/src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const baseUrl = `http://localhost:${port}`;
 
-app.set('views', path.resolve(__dirname, '../views/pages/'));
+app.set('views', path.resolve(__dirname, '../../frontend/views/pages/'));
 app.set('view engine', 'ejs');
 app.get('/1972/09/11', (req, res, next) => {
   res.status(200).render('index');
