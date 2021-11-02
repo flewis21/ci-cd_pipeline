10ddadc9eb510232f3f6d97e0bd846cca5562a67
flewis21 <fabian.lewis@outlook.com>
2021-11-02 19:53:16 -0400

    src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,16 +5,17 @@ const app = express();
 const port = process.env.PORT || 4000;
 const baseUrl = `http://localhost:${port}`;
 
-app.set('static', path.resolve(__dirname, '../../frontend/public/css/style.css'));
+app.set('public_html', path.resolve(__dirname, '../../frontend/public/'));
 app.set('views', path.resolve(__dirname, '../../frontend/views/pages/'));
 app.set('view engine', 'ejs');
 
 // One main middleware for / using express.static and res.render.
 app.use('/1972/09/11', [
   // Use express.static first to look for a static resource.
-  express.static(app.get('static')),
+  express.static(app.get('public_html')),
 
   // If not found render main index, but only for / else next.
+  express.static('public_html'),
   function (req: any, res: any, next: any) {
     if (req.url === '/') {
       res.status(200).render('index', {
