a50a4a310bade606fe03d17eb1829496c6b7d915
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-02 11:58:39 +0000

    src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,13 +5,28 @@ const app = express();
 const port = process.env.PORT || 4000;
 const baseUrl = `http://localhost:${port}`;
 
+app.set('public_html', path.resolve(__dirname, '../../frontend/public/'));
 app.set('views', path.resolve(__dirname, '../../frontend/views/pages/'));
 app.set('view engine', 'ejs');
-app.get('/1972/09/11', (req, res, next) => {
-  res.status(200).render('index', {
-    title: 'My Family Tree - Part I',
-    short_description: 'Pro Bono Volunteer Agency',
-  });
+
+// One main middleware for / using express.static and res.render.
+app.use('/1972/09/11', [
+  // Use express.static first to look for a static resource.
+  express.static(app.get('public_html')),
+
+  // If not found render main index, but only for / else next.
+  function (req: any, res: any, next: any) {
+    if (req.url === '/') {
+      res.status(200).render('index', {
+        title: 'My Family Tree - Part I',
+        short_description: 'Pro Bono Volunteer Agency',
+      });
+    }
+  },
+]);
+
+app.get('*', function (req, res) {
+  res.status(404).render('error', {});
 });
 
 // Server
