aa7413e6913032fd539a58ee272a35170d44cdf7
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-03 19:42:09 +0000

    src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 export {};
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+module.exports.myScript_Src = (alert: any) => {
+  let js = 'amazing';
+  if (js === 'amazing') alert('JavaScript is FUN!');
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+  40 + 8 + 23 - 10;
+  console.log(40 + 8 + 23 - 10);
+};
