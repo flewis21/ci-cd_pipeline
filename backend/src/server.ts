938502fba63b693dab4f66da7734bb1d556c7764
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-04 15:36:53 +0000

    src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import serverless from 'serverless-http';
 import path from 'path';
 const app = express();
-const port = process.env.PORT ?? 4000;
+let port = process.env.PORT || 4000;
 const baseUrl = `http://localhost:${port}`;
 
 app.set('public_html', path.resolve(__dirname, '../../frontend/public/'));
@@ -10,7 +10,7 @@ app.set('views', path.resolve(__dirname, '../../frontend/views/pages/'));
 app.set('view engine', 'ejs');
 
 // One main middleware for / using express.static and res.render.
-app.use('/1972/09/11', [
+app.use('/1972/09/11/', [
   // Use express.static first to look for a static resource.
   express.static(app.get('public_html')),
 
