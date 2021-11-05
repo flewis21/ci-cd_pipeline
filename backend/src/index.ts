b7b8055b0e052bf7e9ce4cd6fc2a3ab93178b58f
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-05 04:47:09 +0000

    src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 export {};
 const js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Fabian`');
+
+let firstname = 'Fabian';
