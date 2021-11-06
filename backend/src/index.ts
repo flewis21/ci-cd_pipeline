3a9b73f8f48a1ccae30e6ba35d9ca1e674ed855a
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-06 19:06:47 +0000

    backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,11 @@
-// Load the http module to create an http server.
-import * as http from 'http';
+let js = 'amazing';
 
-// create a function to handle every HTTP request.
-function handler(req: any, res: any) {
-  var form = '';
+console.log(40 + 8 + 23 - 10);
 
-  if (req.method == 'GET') {
-    form =
-      '<!doctype html> \
-<html lang="en"> \
-<head> \
-	<meta charset="UTF-8"> \
-	<title>JavaScript Fundamentals</title> \
-</head> \
-<body> \
-	<h1>JavaScript Fundamentals - Part I</h1> \
-	<script> \
-		let js = "amazing"; \
-		console.log(40 + 8 + 23 - 10); \
-		console.log("Fabian"); \
-		console.log(23); \
-		let firstname = "Fabian"; \
-		console.log(firstname); \
-	</script> \
-</body> \
-</html>';
+console.log('Fabian');
 
-    // respond
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200);
-    res.end(form);
-  }
-}
+console.log(23);
 
-// Create a serever that invokes `handler` function upon receiving a request
-http.createServer(handler).listen(3999, function (err?: any) {
-  if (err) {
-    console.log('Error starting http server ');
-  } else {
-    console.log('server running at http://127.0.0.1:3999/ or http://localhost:3999/');
-  }
-});
+let firstname = 'Fabian';
+
+console.log(firstname);
