1a19569a76a8496b75e451b22d256a9879fc9a28
flewis21 <fabian.lewis@outlook.com>
2021-11-08 12:47:57 -0500

    backend/src/solc_example.ts

--- backend/src/solc_example.ts
+++ backend/src/solc_example.ts
@@ -2,7 +2,7 @@ import * as solc from 'solc';
 
 let solc_version = 'v0.4.16+commit.d7661dd9';
 
-solc.loadRemoteVersion(solc_version, function (err, solc_specific) {
+solc.loadRemoteVersion(solc_version, function (err: Error, solc_specific: any) {
   if (!err) {
     let output = solc_specific.compile('contract t { function g() {} }', 1);
   }
