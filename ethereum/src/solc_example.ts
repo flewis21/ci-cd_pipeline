b52d78582cece06520fbc8f4bfba4dd0cc5b8abb
flewis21 <fabian.lewis@outlook.com>
2021-11-09 02:59:50 -0500

    src/solc_example.ts

--- ethereum/src/solc_example.ts
+++ ethereum/src/solc_example.ts
@@ -1,16 +1,16 @@
-import * as solc from 'solc';
+import solc from 'solc';
 import * as fs from 'fs';
 
-let solc_version = 'v0.4.16+commit.d7661dd9';
+const solc_version = 'v0.4.16+commit.d7661dd9';
 const contracts_directory = '../../ethereum/contracts';
 const contract_name = 'myContract';
 const contract_filename = 'myContract.sol';
-let is_Optimized = 1;
+const is_Optimized = 1;
 
 const input = {};
 
 solc.loadRemoteVersion(solc_version, function (err: Error, solc_specific: any) {
   if (!err) {
-    let output = solc_specific.compile('contract t { function g() {} }', 1);
+    const output = solc_specific.compile('contract t { function g() {} }', 1);
   }
 });
