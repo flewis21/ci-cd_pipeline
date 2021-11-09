c00776293cc2cf4b36754f88e4bf962dc2b79cbc
flewis21 <fabian.lewis@outlook.com>
2021-11-09 17:30:53 -0500

    backend/src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,14 +19,17 @@ app.use('/', [
   function (req: any, res: any, next: any) {
     if (req.url === '/') {
       express.static('public_html'),
-        res.status(200).render('index', {
+        res.status(200).render('_index', {
           title: 'My Family Tree - Part I',
+          author: 'Pro Bono Volunteer Agency',
           short_description: 'Pro Bono Volunteer Agency',
           sipoc_form:
             'https://docs.google.com/forms/d/e/1FAIpQLSf7eQty92OWCEw_YZ4Irf-PpjYboaKysgVLIBnxESn-TcRfnQ/viewform?embedded=true',
           sipoc_slide:
             'https://docs.google.com/presentation/d/e/2PACX-1vSq6HBWVkXDbQatjrtodxKlGSDJkrj0Uc70jRN6vhzDmvW3RPjysRGhnjCyGZ77vaKLl0IVqB3UDQO3/embed?start=false&loop=false&delayms=60000',
           myScript: 'output/index.js',
+          myCss: 'css/style.css',
+          css_type: 'text/css',
           script_type: 'module',
         });
     }
