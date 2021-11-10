4ce4ee8750c09b7b2021664104e188b69cfbaa7e
flewis21 <80494698+flewis21@users.noreply.github.com>
2021-11-10 00:17:35 -0500

    Delete hello.js

--- backend/src/hello.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-exports.__esModule = true;
-var tslib_1 = require('tslib');
-var ejs_1 = (0, tslib_1.__importDefault)(require('ejs'));
-var html = ejs_1['default'].render('<p> hello <%= name %> </p>', {name: 'ejs'});
-console.log(html); // <p> hello ejs </p>
