edea58a4035af5efc9be0d34f01b7f34084d064c
flewis21 <80494698+flewis21@users.noreply.github.com>
2021-11-10 20:16:31 -0500

    Delete package/frontend/public/output directory

--- package/frontend/public/output/backend/src/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var ejs_1 = __importDefault(require("ejs"));
-var html = ejs_1["default"].render('<p> hello <%= name %> </p>', { name: 'ejs' });
-console.log(html); // <p> hello ejs </p>
