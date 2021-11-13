2ff8b7163253cf000b9e975e7ff1400640c247b6
flewis21 <fabian.lewis@outlook.com>
2021-11-13 01:24:41 -0500

    package/frontend/ethereum/backend/src/web3Dev.ts

--- package/frontend/ethereum/backend/src/web3Dev.ts
+++ package/frontend/ethereum/backend/src/web3Dev.ts
@@ -1,1 +1,9 @@
-import * as web3 from 'web3';
+import dotenv from 'dotenv';
+import Web3 from 'web3';
+const ropsten = process.env.ropsten1;
+const rpcURL = ropsten;
+const web3 = new Web3(rpcURL);
+const address = "0x90e63c3d53E0Ea496845b7a03ec7548B70014A91";
+web3.eth.getBalance(address, (err, wei) => {
+	let balance = web3.utils.fromWei(wei, 'ether')
+})
