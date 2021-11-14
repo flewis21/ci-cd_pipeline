71358cd95ad94f51c798f62e05593203c0735728
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-14 01:13:58 +0000

    package/frontend/ethereum/backend/src/.

--- package/frontend/ethereum/backend/src/web3Dev.ts
+++ package/frontend/ethereum/backend/src/web3Dev.ts
@@ -16,7 +16,15 @@ const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07';
 
 const contract = new web3.eth.Contract(abi, address);
 
-contract.methods.totalSupply().call((err, result) => { console.log(result) });
-contract.methods.name().call((err, result) => { console.log(result) });
-contract.methods.symbol().call((err, result) => { console.log(result) });
-contract.methods.balancOf(address).call((err, result) => { console.log(result) });
+contract.methods.totalSupply().call((err, result) => {
+  console.log(result);
+});
+contract.methods.name().call((err, result) => {
+  console.log(result);
+});
+contract.methods.symbol().call((err, result) => {
+  console.log(result);
+});
+contract.methods.balancOf(address).call((err, result) => {
+  console.log(result);
+});
