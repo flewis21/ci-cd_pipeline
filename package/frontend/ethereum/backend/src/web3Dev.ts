4a2a99f377d2f60aba1f7c9094c01f0ddc7feaed
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-14 23:51:44 +0000

    package/frontend/ethereum/backend/src/web3Dev.ts

--- package/frontend/ethereum/backend/src/web3Dev.ts
+++ package/frontend/ethereum/backend/src/web3Dev.ts
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
-import{ AbiItem } from 'web3-utils'; 
+import ethers from 'ethers';
 import dotenv from 'dotenv';
 dotenv.config();
 const rpcURL = process.env.INFURA_1;
@@ -12,6 +12,7 @@ web3.eth.getBalance(address, (err, wei) => {
 })
 */
 
+/*
 const abi = process.env.OMISEGO_TOKEN_1;
 const address = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07';
 
@@ -29,3 +30,5 @@ contract.methods.symbol().call((err, result) => {
 contract.methods.balancOf(address).call((err, result) => {
   console.log(result);
 });
+*/
+
