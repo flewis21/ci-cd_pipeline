8cde03d37b8c8f5ef0cd942b0d2c41c6a16a0744
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-15 05:48:13 +0000

    backend/src/hardhat.config.ts

--- package/frontend/ethereum/backend/src/hardhat.config.ts
+++ package/frontend/ethereum/backend/src/hardhat.config.ts
@@ -4,4 +4,12 @@
 module.exports = {
   solidity: '0.7.3',
 };
-import * as hardhat from 'hardhat';
+import {HardhatUserConfig} from 'hardhat/config';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.5.12',
+  },
+};
+
+export default config;
