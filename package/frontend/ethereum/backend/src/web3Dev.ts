aaa1463f50f0792f75b8f3aafe5433a072020a4f
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-15 19:52:13 +0000

    package/frontend/ethereum/backend/src/web3Dev.ts

--- package/frontend/ethereum/backend/src/web3Dev.ts
+++ package/frontend/ethereum/backend/src/web3Dev.ts
@@ -1,9 +1,11 @@
 import Web3 from 'web3';
-import ethers from 'ethers';
 import dotenv from 'dotenv';
 dotenv.config();
+
+/*
 const rpcURL = process.env.INFURA_1;
 const web3 = new Web3(rpcURL);
+*/
 
 /*
 const address = "0x90e63c3d53E0Ea496845b7a03ec7548B70014A91";
@@ -32,3 +34,12 @@ contract.methods.balancOf(address).call((err, result) => {
 });
 */
 
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_ID_1));
+
+web3.eth.getBalance('0x5A0b54D5dc17e0AadC383d2db43B0a0D3E029c4c', function (err, result) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(web3.utils.fromWei(result, 'ether') + ' ETH');
+  }
+});
