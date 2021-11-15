e68ac6d7e1fd38ce70f417c5f519c8f87d00aa6c
CMO: C-Suite <f.lewis20.fjjl365@outlook.com>
2021-11-15 02:46:24 +0000

    package/frontend/ethereum/backend/src/ether_index.ts

--- package/frontend/ethereum/backend/src/ether_index.ts
+++ package/frontend/ethereum/backend/src/ether_index.ts
@@ -3,10 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const url = process.env.INFURA_1;
+const provider = ethers.getDefaultProvider();
 const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
 customHttpProvider.getBlockNumber().then((result) => {
 	console.log("Current block number: " + result);
-});
-.catch((error) 	=> {
-	conole.log(error);
 });
