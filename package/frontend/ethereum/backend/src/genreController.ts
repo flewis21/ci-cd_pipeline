f74e505f87cccf42e7f01334c3ab2f02b603a22a
lost-chances <methodology1.slack@gmail.com>
2021-11-22 21:03:33 +0000

    package/frontend/ethereum/backend/src/.

---
+++ package/frontend/ethereum/backend/src/genreController.ts
@@ -0,0 +1,6 @@
+import geneology from './geneology';
+
+// Dispay the blog.
+exports.blog = function (req, res) {
+  res.send('NOT IMPLEMENTED: Blog');
+};
---
+++ package/frontend/ethereum/backend/src/routes.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Require controller modules.
+import genre_controller from './genreController'
