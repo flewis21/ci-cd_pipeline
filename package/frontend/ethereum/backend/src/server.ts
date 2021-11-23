d71bb73d322d1edc75d01d8efade39ef1f597927
lost-chances <methodology1.slack@gmail.com>
2021-11-23 00:36:26 +0000

    package/frontend/ethereum/backend/src/.

--- package/frontend/ethereum/backend/src/server.ts
+++ package/frontend/ethereum/backend/src/server.ts
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
+import createError from 'http-errors';
 import express from 'express';
 import router from './routes';
 import serverless from 'serverless-http';
 import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,6 +16,12 @@ const script = process.env.SCRIPT_2;
 app.set('public_html', path.resolve(__dirname, '../../../public/'));
 app.set('views', path.resolve(__dirname, '../../../views/pages/'));
 app.set('view engine', 'ejs');
+
+// Add middleware libraries.
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // One main middleware for / using express.static and res.render.
 app.use('/1972/09/11', [
@@ -51,17 +60,30 @@ router.get('*', function (req, res, next) {
   });
 });
 
-app.get('*', function (req, res) {
-  res.status(404).render(process.env.WEBSITE_3 || 'error', {
-    title: '404!',
-    s_type_1: '??',
-    script_1: '??',
-    c_type_1: 'text/css',
-    css_1: '1972/09/11/css/style.css',
-    desc_1: 'Error',
-    author_1: 'Page',
-    keywords_1: 'error',
-  });
+// Catch 404  and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// Error handling
+app.use(function(err, req, res, next) {
+    // Set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // Render the error page
+    // app.get('*', function (req, res) {
+    res.status(err.status || 500).render(process.env.WEBSITE_3 || 'error', {
+        title: '404!',
+        s_type_1: '??',
+        script_1: '??',
+        c_type_1: 'text/css',
+        css_1: '1972/09/11/css/style.css',
+        desc_1: 'Error',
+        author_1: 'Page',
+        keywords_1: 'error',
+    });
+    // });
 });
 
 // Server
