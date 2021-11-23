93cc66632c7b8ce9daae63482191da2433e40a2e
lost-chances <methodology1.slack@gmail.com>
2021-11-23 20:37:35 +0000

    package/frontend/ethereum/backend/src/.

--- package/frontend/ethereum/backend/src/routes.ts
+++ package/frontend/ethereum/backend/src/routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 // Require controller modules.
-import genre_controller from '../../public/output/genreController';
+import genre_controller from './genreController';
 
 // Page routes.
 
