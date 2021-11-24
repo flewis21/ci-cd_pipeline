b8752bf1e9083d2430d76b39d8e7c58cce398894
lost-chances <methodology1.slack@gmail.com>
2021-11-24 01:49:17 +0000

    src/.

--- package/frontend/ethereum/backend/src/routesDefault.ts
+++ package/frontend/ethereum/backend/src/routesDefault.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 const router = express.Router();
-const app = express();
 
 // Use express.static first to look for a static resource.
-express.static(app.get('public_html'));
+express.static(router.get('public_html'));
 
 module.exports = router;
