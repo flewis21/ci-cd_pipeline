8b161df8d0403e190405ca731041815db5a5e336
lost-chances <methodology1.slack@gmail.com>
2021-12-01 17:53:56 +0000

    src/.

--- package/frontend/ethereum/backend/src/routesBlogs.ts
+++ package/frontend/ethereum/backend/src/routesBlogs.ts
@@ -11,8 +11,8 @@ blogsRouter.get('/', function (req: any, res: any, next: any) {
   if (req.url === '/') {
     res.status(200).render(process.env.WEBSITE_1 || 'first', {
       title: 'BLOG!',
-      s_type_1: '??',
-      script_1: '??',
+      s_type_1: 'text/javascript',
+      script_1: 'output/index',
       c_type_1: 'text/css',
       css_1: 'css/style.css',
       desc_1: 'Error',
