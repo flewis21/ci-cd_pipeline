53e22f9b7a949ce11270d4b4562c6e4f57aa93d0
flewis21 <fabian.lewis@outlook.com>
2021-12-11 03:40:10 +0000

    ethereum/backend/src/.

---
+++ package/frontend/ethereum/backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+let lastCustomerNumber: any = null;
+
+app.post('/', (req, res) => {
+  lastCustomerNumber = req.body.customerNumber;
+  res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
+  res.send(lastCustomerNumber);
+});
+
+app.listen(4000);
+
+module.exports = app;
