e67825b205f14378b5153a4d6b8b81cdc892c99f
flewis21 <fabian.lewis@outlook.com>
2021-12-14 23:54:37 +0000

    backend/src/.

--- package/frontend/ethereum/backend/src/bscoin.ts
+++ package/frontend/ethereum/backend/src/bscoin.ts
@@ -1,6 +1,12 @@
 import sha256 from 'crypto-js/sha256';
 class BlockCrypto {
-  constructor(index: unknown, current_time: unknown, info: unknown, nextHash = ' ') {
+  index: any;
+  current_time: unknown;
+  info: unknown;
+  nextHash: any;
+  hash: unknown;
+
+  constructor(index: any, current_time: unknown, info: unknown, nextHash: any = ' ') {
     this.index = index;
     this.current_time = current_time;
     this.info = info;
@@ -8,29 +14,34 @@ class BlockCrypto {
     this.hash = this.computeHash();
   }
   computeHash() {
-    return sha256(this.info + this.nextHash + this.current_time + JSON.stringify(this.info)).toString();
+    return sha256(this.index + this.nextHash + this.current_time + JSON.stringify(this.info)).toString();
   }
 }
 class Blockchain {
+  block1chain: any;
+  difficulty: unknown;
+
   constructor() {
-    runInThisContext.block1chain = [this.startGenesisBlock()];
+    this.block1chain = [this.initGenesisBlock()];
+    this.difficulty = 4;
   }
   initGenesisBlock() {
-    return new BlockCrypto(0, '12/13/2021', 'Initial Block in the Chain', '0');
+    return new BlockCrypto(0, '12/13/2021', 'Initial Block in our network', '0');
   }
-  latestBlock() {
+  obtainLatestBlock() {
     return this.block1chain[this.block1chain.length - 1];
   }
   addNewBlock(newBlock) {
-    newBlock.nextHash = this.latestBlock().hash;
+    newBlock.nextHash = this.obtainLatestBlock().hash;
     newBlock.hash = newBlock.computeHash();
     this.block1chain.push(newBlock);
   }
+
   checkValidity() {
     // Checking validity
     for (let i = 1; i < this.block1chain.length; i++) {
       const currentBlock = this.block1chain[i];
-      const nextBlock = this.blockchain[i - i];
+      const nextHash = this.block1chain[i - i];
       // Checking current block hash
 
       if (currentBlock.hash !== currentBlock.computeHash()) {
@@ -38,7 +49,7 @@ class Blockchain {
       }
       // Comparing current block hash with the next block
 
-      if (currentBlock.nextHash !== nextBlock.hash) {
+      if (currentBlock.nextHash !== nextHash.hash) {
         return false;
       }
       return true;
@@ -47,7 +58,9 @@ class Blockchain {
 }
 let bscoin = new Blockchain();
 bscoin.addNewBlock(new BlockCrypto(1, '11/13/2021', {sender: 'Black Daymare', recipient: 'Kyle Smith', quantity: 20}));
-bscoin.addNewBlock(new BlockCrypto(2, '12/13/2021', {sender: 'Brother Love', recpient: 'Shamon Brown', quantity: 349}));
+bscoin.addNewBlock(
+  new BlockCrypto(2, '12/13/2021', {sender: 'Brother Love', recipient: 'Shamon Brown', quantity: 349})
+);
 console.log(JSON.stringify(bscoin, null, 4));
 
 module.exports = {
