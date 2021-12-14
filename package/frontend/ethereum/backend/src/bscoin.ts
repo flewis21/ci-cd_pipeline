9de0ebb0344cf7cbee0b5adb23ae66d957a1bc71
flewis21 <fabian.lewis@outlook.com>
2021-12-14 01:24:31 +0000

    backend/src/.

--- package/frontend/ethereum/backend/src/bscoin.ts
+++ package/frontend/ethereum/backend/src/bscoin.ts
@@ -1,4 +1,5 @@
 import {sha256} from 'crypto-js/sha256';
+import {runInThisContext} from 'vm';
 class BlockCrypto {
   constructor(index, current_time, info, nextHash = ' ') {
     this.index = index;
@@ -9,5 +10,27 @@ class BlockCrypto {
   }
   computeHash() {
     return sha256(this.info + this.nextHash + this.current_time + JSON.stringify(this.info)).toString();
+  }
+}
+class Blockchain {
+  constructor() {
+    runInThisContext.block1chain = [this.startGenesisBlock()];
+  }
+  initGenesisBlock() {
+    return new BlockCrypto(0, '12/13/2021', 'Initial Block in the Chain', '0');
+  }
+  latestBlock() {
+    return this.block1chain[this.block1chain.length - 1];
+  }
+  addNewBlock(newBlock) {
+    newBlock.nextHash = this.latestBlock().hash;
+    newBlock.hash = newBlock.computeHash();
+    this.block1chain.push(newBlock);
+  }
+  checkValidity() {
+    // Checking validity
+    for (let i = 1; i < this.block1chain.length; i++) {
+      const currentBlock = this.block1chain[i];
+    }
   }
 }
