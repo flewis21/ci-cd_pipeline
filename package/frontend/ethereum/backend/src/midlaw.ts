77d2833d7be7411630fe01ac0efcc3059b2b5caa
flewis21 <fabian.lewis@outlook.com>
2022-03-02 22:21:36 +0000

    midlaw.ts

--- package/frontend/ethereum/backend/src/midlaw.ts
+++ package/frontend/ethereum/backend/src/midlaw.ts
@@ -1,4 +1,4 @@
-import * from fs as 'fs';
+import * as fs from 'fs';
 import path from 'path';
-import { Template, Clause } from '../../../accordProject/@accordproject/cicero-core';
-import { Engine } from '../../../accordProject/@accordproject/cicero-engine'
+import {Template, Clause} from '../../../accordProject/@accordproject/cicero-core';
+import {Engine} from '../../../accordProject/@accordproject/cicero-engine';
