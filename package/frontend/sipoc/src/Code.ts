67fb0551ac0307981d3dbf461e8a07157a8b06c5
livingWater <f.lewis20.livingwater19@outlook.com>
2022-04-26 20:10:53 +0000

    src

--- package/frontend/sipoc/src/Code.ts
+++ package/frontend/sipoc/src/Code.ts
@@ -1,3 +1,4 @@
+// Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 function doGet(e) {
   var params = JSON.stringify(e);
   return HtmlService.createHtmlOutput(params);
--- package/frontend/sipoc/src/funcs.ts
+++ package/frontend/sipoc/src/funcs.ts
@@ -1,3 +1,5 @@
-function doStuff(){
-    logger.log("Doing stuff")
+// Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
+function doStuff() {
+  Logger.log('Doing stuff');
+  Logger.log('Changes on the Server');
 }
