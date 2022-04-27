2ad4c8a37fe266ffc52706cfb7914393417884c1
flewis21 <80494698+flewis21@users.noreply.github.com>
2022-04-26 21:33:27 -0400

    edit

--- package/frontend/sipoc/src/Code.ts
+++ package/frontend/sipoc/src/Code.ts
@@ -1,15 +1,18 @@
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 function doGet() {
+  //SlidesApp.getUi();
+  //DriveApp.getRootFolder();
+  //UrlFetchApp.fetch("");
   return HtmlService.createTemplateFromFile('index').evaluate();
 }
 
-function doGet(e) {
+function params(e) {
   var params = JSON.stringify(e);
   return HtmlService.createHtmlOutput(params);
 }
 
 function addToSideBar() {
-  const htmlServ = HtmlService.createTemplateFromFile('my');
+  const htmlServ = HtmlService.createTemplateFromFile('index');
   const html = htmlServ.evaluate();
 
   const ui = FormApp.getUi();
