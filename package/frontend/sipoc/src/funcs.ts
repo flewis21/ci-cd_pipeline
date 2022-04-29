4ac0612a8ddaba3e20487ef49f6c40207121f06b
lost chances <methodology1.slack@gmail.com>
2022-04-29 18:29:24 +0000

    funcs.ts

--- package/frontend/sipoc/src/funcs.ts
+++ package/frontend/sipoc/src/funcs.ts
@@ -10,6 +10,15 @@ function scriptJs(name) {
   let country = 'USA';
   let continent = 'North America';
   let population = 331.45 * 1000000;
+  let johnMass = 92;
+  let johnHeight = 1.95;
+  let markMass = 78;
+  let markHeight = 1.69;
+  // BMI = mass / height ** 2 = mass / (height * height);
+  let johnBMI = johnMass / johnHeight ** 2;
+  let markBMI = markMass / (markHeight * markHeight);
+  if (markBMI > johnBMI) markHigherBMI = true;
+  else markhigherBMI = false;
 
   // Logger.log(name + " clicked the button");
   // Logger.log(country);
