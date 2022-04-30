287b5f6d2ffe582a8947d93b8482deaf149192c2
lost chances <methodology1.slack@gmail.com>
2022-04-30 07:04:30 +0000

    src/

--- package/frontend/sipoc/src/Code.ts
+++ package/frontend/sipoc/src/Code.ts
@@ -1,11 +1,14 @@
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 function doGet(e) {
-  Logger.log(e.parameter);
   //SlidesApp.getUi();
   //DriveApp.getRootFolder();
   //UrlFetchApp.fetch("");
-  return HtmlService.createHtmlOutputFromFile('index');
+  const htmlOutput = HtmlService.createTemplateFromFile('index');
+  return htmlOutput.evaluate();
+}
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 function addToSideBar() {
   var htmlServ = HtmlService.createTemplateFromFile('index');
--- package/frontend/sipoc/src/funcs.ts
+++ package/frontend/sipoc/src/funcs.ts
@@ -1,31 +1,9 @@
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 function scriptJs(name) {
-  var url = '';
+  var url = 'https://docs.google.com/spreadsheets/d/1-rMshTSU0TdKsyJOe-NJUbUMLPYSmnnxBk0GvoOzVZ4/edit#gid=0';
   var ss = SpreadsheetApp.openByUrl(url);
-  var ws = ss.getSheetByName('Customers');
+  var ws = ss.getSheetByName('Suppliers');
 
   ws.appendRow([name]);
-
-  let country = 'USA';
-  let continent = 'North America';
-  let population = 331.45 * 1000000;
-  let johnMass = 92;
-  let johnHeight = 1.95;
-  let markMass = 78;
-  let markHeight = 1.69;
-  // BMI = mass / height ** 2 = mass / (height * height);
-  let johnBMI = johnMass / johnHeight ** 2;
-  Logger.log("John's BMI is " + johnBMI);
-  let markBMI = markMass / (markHeight * markHeight);
-  Logger.log("Mark's BMI is " + markBMI);
-  let markHigherBMI;
-  if (markBMI > johnBMI) markHigherBMI = true;
-  else markHigherBMI = false;
-  Logger.log("Marks's BMI greater than John's is " + markHigherBMI);
-
-  // Logger.log(name + " clicked the button");
-  // Logger.log(country);
-  // Logger.log(continent);
-  // Logger.log(population);
 }
