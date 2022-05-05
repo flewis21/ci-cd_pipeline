c97ee956134abcb53992130fcc1f38169e87bc31
flewis21 <80494698+flewis21@users.noreply.github.com>
2022-05-04 20:40:13 -0400

    Update funcs.ts

--- package/frontend/sipoc/src/funcs.ts
+++ package/frontend/sipoc/src/funcs.ts
@@ -1,7 +1,7 @@
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 // Compiled using sipoc 1.0.0 (TypeScript 4.6.3)
 function scriptJs(name) {
-  const url = 'https://docs.google.com/spreadsheets/d/1-rMshTSU0TdKsyJOe-NJUbUMLPYSmnnxBk0GvoOzVZ4/edit#gid=0';
+  const url = '';
   const ss = SpreadsheetApp.openByUrl(url);
   const ws = ss.getSheetByName('Suppliers');
 
